97351f53c89368ae838fdd26267583b272bfc010
Gareth Edwards <gareth@softwire.com>
2020-12-06 17:52:13 +0000

    Days 5+6

---
+++ Day5.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020 {
+
+    class Day5 : Day
+    {
+        public override int Part1(string input)
+        {
+            return Utils.splitLines(input).Select(l => idFromCode(l)).Max();
+        }
+
+        public override int Part2(string input)
+        {
+            int i1 = -99;
+
+            foreach (var i2 in Utils.splitLines(input).Select(l => idFromCode(l)).OrderBy(i => i)) {
+                if (i2 - i1 == 2) 
+                {
+                    return i1 + 1;
+                }
+                i1 = i2;
+
+
+            }
+
+            return -1;
+            
+        }
+
+/*
+    BFFFBBFRRR: row 70, column 7, seat ID 567.
+    FFFBBBFRRR: row 14, column 7, seat ID 119.
+    BBFFBBFRLL: row 102, column 4, seat ID 820.
+*/
+
+        [Test(567, "BFFFBBFRRR")]
+        int idFromCode(string seatcode)
+        {
+            return parseCode(seatcode).id;
+        }
+
+        Seat parseCode(string seatcode)
+        {
+            var r = Convert.ToInt32(seatcode.Substring(0, 7).Replace("B", "1").Replace("F", "0"),2);
+            var c = Convert.ToInt32(seatcode.Substring(7).Replace("R", "1").Replace("L", "0"),2);
+
+            return new Seat(r,c);
+            
+        }
+
+        record Seat(int r, int c)
+        {
+            public int id = r * 8 + c;        
+        }
+
+    }
+}
---
+++ Day6.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020 {
+
+    class Day6 : Day
+    {
+        [Test(11, testData)]
+        public override int Part1(string input)
+        {
+            return Calc(input, true);
+        }
+
+        public int Calc(string input, bool combine)
+        {
+            HashSet<char> s = null;
+            var c = 0;
+
+            foreach (var l in Utils.splitLines(input))
+            {
+                if (l == "")
+                {
+                    c += s.Count;
+                    s = null;
+                }
+                else 
+                {
+                    var s2 = l.ToArray().ToHashSet();
+                    if (s == null)
+                    {
+                        s = s2;
+                    }
+                    else
+                    {
+                        if (combine) {
+                            s.UnionWith(s2);
+                        }
+                        else 
+                        {
+                            s.IntersectWith(s2);
+                        }
+
+                    }
+                }
+            }
+
+            if (s != null)
+            {
+                c += s.Count;
+            }
+
+            return c;            
+        }
+
+        public override int Part2(string input)
+        {
+            return Calc(input, false);
+        }
+
+        const string testData = 
+@"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+    }
+}
